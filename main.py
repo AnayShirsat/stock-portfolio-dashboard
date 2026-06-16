4abaca1fe4f1b1bcdaed5af5f630605e9269eb83
AnayShirsat <YOUR_GITHUB_EMAIL>
2026-06-17 00:28:54 +0530

    replace main with streamlit dashboard

--- main.py
+++ main.py
@@ -1,51 +1,165 @@
+import os
+import matplotlib.pyplot as plt
 import yfinance as yf
-portfolio = []
+import streamlit as st
+import pandas as pd
 
-while True:
+if "portfolio" not in st.session_state:
 
-    stock_name = input("Enter stock name : ")
-    qty = int(input("Enter quantity : "))
-    buy_price = float(input("Enter buy price : "))
-  
-    ticker = yf.Ticker(stock_name + ".NS")
+    if os.path.exists("portfolio.csv"):
+
+        st.session_state.portfolio = pd.read_csv(
+            "portfolio.csv"
+        ).to_dict("records")
+
+    else:
+
+        st.session_state.portfolio = []
+
+ #for stock in portfolio:
+ #   stock["Profit"] = (
+  #      stock["Qty"] * stock["Current"]
+   #     - stock["Qty"] * stock["Buy"]
+    #)
+
+total_value = 0
+total_profit = 0
+
+for stock in st.session_state.portfolio:
+    total_value += stock["Qty"] * stock["Current"]
+    total_profit += stock.get("Profit", 0)
+
+st.title("📈 Stock Portfolio Dashboard")
+
+stock_name = st.text_input("Stock Name")
+
+qty = st.number_input("Quantity", min_value=1)
+
+buy_price = st.number_input("Buy Price", min_value=0.0)
+
+if st.button("Add Stock"):
+
+    ticker = yf.Ticker(stock_name.upper() + ".NS")
+
     current_price = ticker.history(period="1d")["Close"].iloc[-1]
 
-    print("Live Price:", current_price)
+    profit = (qty * current_price) - (qty * buy_price)
 
     stock = {
-        "stock": stock_name,
-        "qty": qty,
-        "buy": buy_price,
-        "current": current_price
+        "Stock": stock_name.upper(),
+        "Qty": qty,
+        "Buy": buy_price,
+        "Current": round(current_price, 2),
+        "Profit": round(profit, 2)
     }
 
-    portfolio.append(stock)
+    st.session_state.portfolio.append(stock)
 
-    choice = input("Add another stock? (y/n): ")
+    df_save = pd.DataFrame(st.session_state.portfolio)
 
-    if choice.lower() == "n":
-        break
+    df_save.to_csv("portfolio.csv", index=False)
 
-print(portfolio)
-total_invested = 0
-total_current = 0
+    st.write("Portfolio Length:", len(st.session_state.portfolio))
 
-for stock in portfolio:
+if st.button("🔄 Refresh Prices"):
 
-    invested = stock["qty"] * stock["buy"]
-    current_value = stock["qty"] * stock["current"]
-    profit = current_value - invested
+    for stock in st.session_state.portfolio:
 
-    total_invested += invested
-    total_current += current_value
+        ticker = yf.Ticker(stock["Stock"] + ".NS")
 
-    print("\nStock:", stock["stock"])
-    print("Invested: ₹", round(invested, 2))
-    print("Current Value: ₹", round(current_value, 2))
-    print("Profit: ₹", round(profit, 2))
-    total_profit = total_current - total_invested
+        current_price = ticker.history(period="1d")["Close"].iloc[-1]
 
-print("\nPortfolio Summary")
-print("Total Invested: ₹", round(total_invested, 2))
-print("Total Current Value: ₹", round(total_current, 2))
-print("Total Profit: ₹", round(total_profit, 2))
+        stock["Current"] = round(current_price, 2)
+
+        stock["Profit"] = round(
+            (stock["Qty"] * current_price)
+            - (stock["Qty"] * stock["Buy"]),
+            2
+        )
+
+    pd.DataFrame(
+        st.session_state.portfolio
+    ).to_csv(
+        "portfolio.csv",
+        index=False
+    )
+
+    st.success("Prices Updated!")
+
+    st.rerun()    
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.metric("Portfolio Value", f"₹{total_value:,.2f}")
+
+with col2:
+    st.metric("Total Profit", f"₹{total_profit:,.2f}")
+
+
+st.write("Welcome to my first fintech project 🚀")
+
+df = pd.DataFrame(st.session_state.portfolio)
+
+
+if not df.empty:
+
+   chart_data = df.set_index("Stock")["Profit"]
+
+   st.subheader("Profit by Stock")
+
+   st.bar_chart(chart_data)
+
+   if not df.empty:
+
+    allocation = df["Qty"] * df["Current"]
+
+    fig, ax = plt.subplots()
+
+    ax.pie(
+        allocation,
+        labels=df["Stock"],
+        autopct="%1.1f%%"
+    )
+
+    ax.set_title("Portfolio Allocation")
+
+    st.pyplot(fig)
+
+    st.dataframe(df)
+
+    if not df.empty:
+
+        stock_to_delete = st.selectbox(
+        "Select stock to delete",
+        df["Stock"]
+    )
+
+    if st.button("Delete Stock"):
+
+        st.session_state.portfolio = [
+            stock
+            for stock in st.session_state.portfolio
+            if stock["Stock"] != stock_to_delete
+        ]
+
+        pd.DataFrame(
+            st.session_state.portfolio
+        ).to_csv(
+            "portfolio.csv",
+            index=False
+        )
+
+        st.rerun()
+
+csv = df.to_csv(index=False)
+
+st.download_button(
+    label="📥 Download Portfolio CSV",
+    data=csv,
+    file_name="portfolio.csv",
+    mime="text/csv"
+)
+
+  
+
